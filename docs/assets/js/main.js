cc6c40ab74412babaf3b9f9174807e190182035c
Sean McPherson <git@seanmcp.com>
2021-09-21 09:14:20 -0400

    commit

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,3 +1,44 @@
-(function main() {
+(async function main() {
+  const { el } = await import("./el.js");
+
   // Code
+  const cardsContainer = document.getElementById("cards-container");
+  const addCardButton = document.getElementById("add-card");
+
+  function addCard({ term = "", body = "" } = {}) {
+    const card = el("div", {
+      className: "card",
+    });
+    const termInput = el("input", {
+      "aria-label": "term",
+      name: "t",
+      required: true,
+      value: term,
+    });
+    const bodyTextarea = el("textarea", {
+      "aria-label": "body",
+      name: "b",
+      required: true,
+      value: body,
+    });
+    const deleteButton = el("button", {
+      className: "subtle",
+      textContent: 'Delete'
+    });
+    deleteButton.addEventListener("click", (event) => {
+      event.preventDefault();
+      card.remove();
+    });
+    card.appendChild(termInput);
+    card.appendChild(bodyTextarea);
+    card.appendChild(deleteButton);
+    cardsContainer.appendChild(card);
+  }
+
+  addCardButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    addCard();
+  });
+
+  addCard();
 })();
